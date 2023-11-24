500f631932dd5de44a322ff05789b5f51f599c4e
Wanderlay <151048965+Wanderlay@users.noreply.github.com>
2023-11-24 15:29:34 -0300

    Trabalho final parte 1.
    Eu adicionei uma navbar, peguei a base do projeto no moodle. Adicionei uma pasta para estilizar futuramente. E comecei o processo para baixar os vídeos no local storage.

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,39 @@
+import React, { useState, useEffect} from "react";
+import {Link} from "react-router-dom";
+
 export default function Home(){
+
+    const alStorage = localStorage.getItem("lista");
+    const [letra, setLetra] = useState("");
+    const [video, setVideo] = useState("");
+    const [nome, setNome] = useState("");
+    const [biblioteca, setBiblioteca] = useState(alStorage ? JSON.parse(alStorage) : []);
+    const [Id, setId] = useState(0)
+
     return(
-        <h1>PTAC 2 - PROJETO FINAL</h1>
+        <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">cadastrar-se/login</a></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Em alta</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled"></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
      );
 }
