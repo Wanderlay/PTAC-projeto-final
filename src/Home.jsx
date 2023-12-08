710e45544fba5af4d03e5598d952773616127892
Wanderley <nuy.yan.ke@gmail.com>
2023-12-08 10:13:07 -0400

    card pronto
    fiz os card e as adicoes

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,12 +3,77 @@ import {Link} from "react-router-dom";
 
 export default function Home(){
 
-    const alStorage = localStorage.getItem("lista");
+    
+    const alStorage = localStorage.getItem("itens");
+    const [biblioteca, setBiblioteca] = useState(alStorage ? JSON.parse(alStorage) : []);
+    const alStorage2 = localStorage.getItem("Compor");
+    const [letras, setLetras] = useState(alStorage ? JSON.parse(alStorage) : []);
     const [letra, setLetra] = useState("");
     const [video, setVideo] = useState("");
     const [nome, setNome] = useState("");
-    const [biblioteca, setBiblioteca] = useState(alStorage ? JSON.parse(alStorage) : []);
-    const [Id, setId] = useState(0)
+    const [Id, setId] = useState(0);
+    const [resumo, setResumo] = useState("");
+    const [autor, setAutor] = useState("")
+
+    useEffect(() => {
+      localStorage.setItem("itens", JSON.stringify(biblioteca));
+      document.title = `Você clicou ${count} vezes`;
+    }, [biblioteca, count]);
+    
+    useEffect(() => {
+      localStorage.setItem("Compor", JSON.stringify(letras));
+    }, [letras]);
+
+    const salvar = (e) => {
+      e.preventDefault();
+      setBiblioteca([
+        ...biblioteca,
+        {
+          conteudo: conteudo,
+          video: video,
+          nome: nome,
+          Id: Id,
+          resumo: resumo,
+          autor: autor,
+          letra: letra
+        },
+      ]);
+      setConteudo("");
+      setNome("");
+      setVideo("");
+      setId(Id + 1);
+      setAutor("");
+      setLetra("");
+      setCount(count + 1);
+    };
+   
+    const salvarLetra = (e) => {
+      e.preventDefault();
+      setLetras([{letra: letra}]);
+     
+    };
+  
+    const apagar = (index) => {
+      const novabiblioteca = [...biblioteca];
+      novabiblioteca.splice(index, 1);
+      setBiblioteca(novabiblioteca);
+    };
+  
+    const apagarLetra = (index) => {
+      const apagar = [];
+     
+      setLetra(apagar);
+    };
+    
+
+
+
+    const MostraLetra = (index) => {
+      const novaletras = [index];
+      
+      setLetras(novaletras);
+    };
+  
 
     return(
        <div> 
@@ -39,6 +104,47 @@ export default function Home(){
        
         </div>
       </nav> 
+
+
+      <form onSubmit={salvar}>
+                <h2>Nome</h2>
+                <input value={nome} onChange={(e) => setNome(e.target.value)} type="text"></input>
+                <h2>Incorporaçao do video:</h2>
+                <input value={video} onChange={(e) => setVideo(e.target.value)} type="text"></input>
+                <h2>Autor</h2>
+                <input value={autor} onChange={(e) => setAutor(e.target.value)} type="text"></input>
+                <h2>Letra</h2>
+                <input value={letra} onChange={(e) => setLetra(e.target.value)} type="text"></input>
+                <h2>Resumo do video</h2>
+                <input value={resumo} onChange={(e) => setResumo(e.target.value)} type="text"></input>
+
+                <button type="submit">ADD</button>
+            </form>
+
+      <div className="container text-center">
+        <div className="row">
+          {biblioteca.map((ativ, index) => (
+            <div className="col" key={index}>
+              <div className="card" style={{ width: "18rem" }}>
+                <div dangerouslySetInnerHTML={{ __html: ativ.video }} />
+                <div className="card-body">
+                  <h5 className="card-title">{ativ.nome}</h5>
+                  <p className="card-text">{ativ.autor}</p>
+                  <p className="card-text">{ativ.resumo}</p>
+                  <div className="azul">
+                    <button id={ativ.Id} onClick={() => apagar(index)} className="btn btn-primary">Apagar</button>
+                    <button id={ativ.letra} onClick={() => salvarLetra(ativ.letra)} className="btn btn-primary">Descrição</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+   
+
+
       </div>
 
       
